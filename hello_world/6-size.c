a212dc74e8a7c6e574481222d4e675b15ed88f5a
laetitiabeles <laetitiabeles9@gmail.com>
2023-10-13 23:06:23 +0200

    prints sizes of types

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,24 +1,26 @@
-#include <stdio.h>                                                          
+#include <stdio.h>
 
 /**
 * main - Entry point
 * Description: the program prints the size of each type using printf
-* sizeof() - prints the type size in bytes                                  * Return: Always 0 (successful)                                             */
-                                                   
+* sizeof() - prints the type size in bytes
+* Return: Always 0 (successful)
+*/
+
 int main(void)
 
 {
 
-printf("Size of a char: %lu byte(s)\n", sizeof(char));                      
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
 
 printf("Size of an int: %lu byte(s)\n", sizeof(int));
 
-printf("Size of a long int: %lu byte(s)\n", sizeof(long int));              
+printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 
-printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));    
+printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 
 printf("Size of a float: %lu byte(s)\n", sizeof(float));
 
 return (0);
 
-} 
+}
