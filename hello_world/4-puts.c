bf1ee4934ebd327a27453f680d3b2f508120122d
laetitiabeles <laetitiabeles9@gmail.com>
2023-10-13 17:16:49 +0200

    fixing my code with betty

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
-  puts(""Programming is like building a multilingual puzzle\n");
-  return 0;
+/**
+ * Fonction principale (main) du programme.
+ *
+ * Cette fonction affiche une chaîne de caractères sur la sortie standard.
+ *
+ * @return 0 en cas de succès.
+ */
+
+int main(void)
+{
+char Mystr[] = "\"Programming is like building a multilingual puzzle";
+
+puts(Mystr);
+
+return (0);
 }
-
