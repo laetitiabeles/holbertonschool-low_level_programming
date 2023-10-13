6053ba50890fbbf9946e631e74c2b7a08b9c6969
laetitiabeles <laetitiabeles9@gmail.com>
2023-10-13 17:42:19 +0200

    program which prints a string with printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main is the entry point
+* the program displays a string with the function printf
+* the program returns 0 if it executes successfull
+*/
+
+int main(void)
+{
+
+printf("with proper grammar, but the outcome is a piece of art,");
+
+return (0);
+
+}
