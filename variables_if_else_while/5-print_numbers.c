dc8919e05f84b11dc4cd14af42b18f8e334340a7
laetitiabeles <laetitiabeles9@gmail.com>
2023-10-16 22:29:37 +0200

    print 0 to 9

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Description: program that prints all single digit starting from 0
+*
+* Return: always 0 (success)
+*/
+
+int main(void)
+{
+
+int n;
+
+for (n = 0 ; n < 9 ; n++)
+{
+
+printf("%d\n", n);
+
+}
+
+return (0);
+
+}
