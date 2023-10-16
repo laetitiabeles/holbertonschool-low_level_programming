d84bbcc5228b08bbfa088d98490b83c95fb1ea4c
laetitiabeles <laetitiabeles9@gmail.com>
2023-10-16 20:48:24 +0200

    fixing gcc

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,27 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 
 /**
 * main - entry point
 *
-* if - condition
+* Description: if - condition
 *
-* return: always 0 (success)
+* Return: always 0 (success)
 */
 
 int main(void)
 {
 
 int n;
+int last_d = n % 10;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* your code goes there */
-int last_d = n % 10;
 
-if (n > 5) 
+if (n > 5)
 {
 printf("Last digit of %d is %d and is greater than 5\n", n, last_d);
 }
