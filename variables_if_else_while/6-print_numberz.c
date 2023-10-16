c9411e8c6d2195273f4e1f035db424c7c49a5e8d
laetitiabeles <laetitiabeles9@gmail.com>
2023-10-16 22:35:56 +0200

    print 0 to 9 with putchar

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Description: program that prints all single digit starting from 0
+*
+* Return: always 0 (success)
+*/
+
+int main(void)
+{
+
+int n;
+
+for (n = 0 ; n < 10 ; n++)
+{
+
+putchar(n);
+
+}
+
+putchar('\n');
+
+return (0);
+
+}
