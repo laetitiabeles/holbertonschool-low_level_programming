d69ede731d37d5a39e8879a0cfa4c23f4eeae67a
laetitiabeles <laetitiabeles9@gmail.com>
2023-10-16 21:18:59 +0200

    fixing gcc

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -13,24 +13,23 @@
 int main(void)
 {
 
-int n;
+int n, last_d;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-
-int last_d = n % 10;
+last_d = n % 10;
 
 /* your code goes there */
 
-if (n > 5)
+if (last_d > 5)
 {
 printf("Last digit of %d is %d and is greater than 5\n", n, last_d);
 }
-else if (n == 0)
+else if (last_d == 0)
 {
 printf("Last digit of %d is %d and is 0\n", n, last_d);
 }
-else if (n < 6 && !0)
+else if (last_d < 6 && last_d != 0)
 {
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_d);
 }
