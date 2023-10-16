66b6adbe28768be6c3dad9a9a0be467c6ad61e61
laetitiabeles <laetitiabeles9@gmail.com>
2023-10-16 23:02:46 +0200

    print all hexa

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Description: program that prints all hexa
+*
+* Return: always 0 (success)
+*/
+
+int main(void)
+{
+
+int n;
+
+for (n = 0 ; n <= 9 ; n++)
+{
+
+putchar('0' + n);
+
+}
+
+for (n = 10 ; n <= 15 ; n++)
+{
+
+putchar('A' + (n - 10));
+
+}
+
+putchar('\n');
+
+return (0);
+
+}
