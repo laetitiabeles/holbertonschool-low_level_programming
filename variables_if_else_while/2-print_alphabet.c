3d2f7f79f36d3f94132039ae2d328c09d098760c
laetitiabeles <laetitiabeles9@gmail.com>
2023-10-16 21:39:48 +0200

    program that print alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Description: program that prints alphabet
+*
+* Return: always 0 (success)
+*/
+
+int main(void)
+{
+
+char ch;
+
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+
+putchar(ch);
+
+}
+
+return (0);
+
+}
