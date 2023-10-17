6ea1c7f15f968fd41da898e57201def78ceba67d
laetitiabeles <laetitiabeles9@gmail.com>
2023-10-17 15:39:36 +0200

    print sign of a number

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* print_sign - Entry point
+* @n: le caractère à vérifier
+* Description: program that check if c is lower or upper
+* Return: always 0
+*/
+
+int print_sign(int n)
+{
+
+if (n > 0)
+{
+return (1 + n);
+}
+else if (n == 0)
+{
+return (0 + n);
+}
+else 
+{
+return (-1 + '/');
+}
+}
