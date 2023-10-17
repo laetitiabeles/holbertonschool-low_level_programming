32cfc6fdf5f0f1053d82231bcb2523692bc48087
laetitiabeles <laetitiabeles9@gmail.com>
2023-10-17 10:26:12 +0200

    fixing output as numbers

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -13,14 +13,14 @@ int main(void)
 
 int n;
 
-for (n = 0 ; n <= 9 ; n++)
+for (n = 0 ; n <= 8 ; n++)
 {
 putchar('0' + n);
 putchar(',');
 putchar(' ');
 }
 
-putchar('\n');
+putchar(' 9');
 
 return (0);
 
