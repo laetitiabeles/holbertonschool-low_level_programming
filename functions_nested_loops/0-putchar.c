2f3ff85efa060fca0f8ecf0c4b4dbe6349fef37f
laetitiabeles <laetitiabeles9@gmail.com>
2023-10-17 13:25:42 +0200

    fixing gcc

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,3 +1,4 @@
+#include <unistd.h>
 #include "main.h"
 
 /**
@@ -11,14 +12,14 @@
 int main(void)
 {
 
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 
-return (0);
+	return (0);
 }
