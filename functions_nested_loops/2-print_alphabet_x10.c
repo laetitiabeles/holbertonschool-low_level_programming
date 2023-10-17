2993b432e168ea3b1ed7a1465656770e9d741fbc
laetitiabeles <laetitiabeles9@gmail.com>
2023-10-17 14:45:08 +0200

    print alphabet x10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* print_alphabet - Entry point
+* Description: program that print alphabet
+* Return: always 0
+*/
+
+void print_alphabet_x10(void)
+{
+
+char c = 'a';
+int i;
+
+for (i = 0; i < 10; i++)
+{
+for (c = 'a' ; c <= 'z' ; c++)
+{
+_putchar(c);
+}
+}
+}
