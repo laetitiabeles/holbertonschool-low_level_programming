660d7d87eb2c6683b490fcbb49257e47b7749b8a
laetitiabeles <laetitiabeles9@gmail.com>
2023-10-17 15:19:05 +0200

    check if lower or upper fixing betty

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+* _islower - Entry point
+* @c: le caractère à vérifier
+* Description: program that check if c is lower or upper
+* Return: always 0
+*/
+
 int _islower(int c)
 {
 
@@ -12,4 +19,4 @@ else
 {
 return (0);
 }
-} 
+}
