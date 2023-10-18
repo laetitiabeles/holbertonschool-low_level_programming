c111a4cadacff5c898aa7a90e8907042efefb316
laetitiabeles <laetitiabeles9@gmail.com>
2023-10-18 13:45:42 +0200

    print to 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -12,7 +12,18 @@ void print_to_98(int n)
 {
 for (n = 0; n < 98 ; n++)
 {
-printf("%d ,", n);
-}
+	if (n > 98)
+	{
+		printf("%d, ",n);
+	}
+	else
+	{
+		printf("%d, ",n);
+	}
 
+if (n == 98)
+{
+printf("%d, ", n);
+}
+}
 }
