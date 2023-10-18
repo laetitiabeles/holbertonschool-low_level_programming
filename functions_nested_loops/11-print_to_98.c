43f3e4e751da367869b32df0f5d91cf1e4c2d5af
laetitiabeles <laetitiabeles9@gmail.com>
2023-10-18 13:38:52 +0200

    print to 98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_to_98 - print while n is not 98
+* @n: number to check
+* Description: program to print n while is not 98
+* Return: Always 0.
+*/
+
+void print_to_98(int n)
+{
+for (n = 0; n < 98 ; n++)
+{
+printf("%d ,", n);
+}
+
+}
