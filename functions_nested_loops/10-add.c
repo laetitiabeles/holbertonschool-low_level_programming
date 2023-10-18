13af00653a8692dbde35a38b5c8273ead069fe74
laetitiabeles <laetitiabeles9@gmail.com>
2023-10-18 13:12:52 +0200

    add 2 int

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* add - add 2 int
+* @num: first int
+* @num2: second int
+* Description: program that add 2 int
+*
+* Return: Always 0.
+*/
+
+int add(int num, int num2)
+{
+
+int result = num + num2;
+
+_putchar('0' + result);
+
+return (0);
+}
