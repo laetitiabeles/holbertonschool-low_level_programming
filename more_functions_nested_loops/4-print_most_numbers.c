5861d9c30300ba640e3ea24a1dd1542c7785fbc1
laetitiabeles <laetitiabeles9@gmail.com>
2023-10-22 16:50:05 +0200

    0 to 9 without 2 and 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* print_most_numbers - entry point
+*
+* Description: program that print 0 to 9 without 2 and 4
+*
+* Return: Always 0
+*/
+
+void print_most_numbers(void)
+{
+int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		if (num == '2' || num == '4')
+		{
+		num++;
+		}
+		_putchar(num);
+	}
+	_putchar('\n');
+}
