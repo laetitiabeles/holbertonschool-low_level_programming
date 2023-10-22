986ca1ab4c020cbf5d2460bb325c3b08b7766ff6
laetitiabeles <laetitiabeles9@gmail.com>
2023-10-22 16:53:22 +0200

    0 to 9 without 2 and 4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -10,15 +10,15 @@
 
 void print_most_numbers(void)
 {
-int num;
+	int num;
 
-	for (num = 0; num <= 9; num++)
+	for (num = '0'; num <= '9'; num++)
 	{
 		if (num == '2' || num == '4')
 		{
 		num++;
 		}
-		_putchar('0' + num);
+		_putchar(num);
 	}
 	_putchar('\n');
 }
