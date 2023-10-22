6e8e3a85de5d678402f831a68def73ad8a3c2f20
laetitiabeles <laetitiabeles9@gmail.com>
2023-10-22 16:25:55 +0200

    mul 2 int

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* mul - entry point
+* @a: first int
+* @b: Second int
+*
+*Return: resultat de la multiplication.
+*/
+
+int mul(int a, int b)
+{
+
+	return (a * b);
+
+}
