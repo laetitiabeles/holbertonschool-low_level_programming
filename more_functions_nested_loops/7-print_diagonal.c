f725fc6a5c0368be066b6a399e2f0229782e78d0
laetitiabeles <laetitiabeles9@gmail.com>
2023-10-22 17:38:12 +0200

    print diagonal fixing esp

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -9,10 +9,18 @@
 
 void print_diagonal(int n)
 {
+
+int slash;
+int esp;
+
 	if (n > 0)
 	{
-		for (; n > 0; n--)
+		for (slash = 1; slash <= n; slash++)
 		{
+			for (esp = 1; esp < slash; esp++)
+			{
+				_putchar(' ');
+			}
 			_putchar('\\');
 			_putchar('\n');
 		}
