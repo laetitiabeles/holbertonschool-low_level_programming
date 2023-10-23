8911dec6cb7804ec2452a007b156ac696672ae03
laetitiabeles <laetitiabeles9@gmail.com>
2023-10-23 13:11:08 +0200

    change value of n

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* reset_to_98 - Entry point
+* @n: pointeur de n
+* Description: program that change the value from 402 to 98
+* Return: always 0
+*/
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
