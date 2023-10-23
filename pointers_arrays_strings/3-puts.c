7cab44ff374d6440983ab0697d69ee4ab7895803
laetitiabeles <laetitiabeles9@gmail.com>
2023-10-23 14:58:12 +0200

    puts string

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _puts - Entry point
+* @str: pointeur
+* Description: program that print a string
+* Return: always 0
+*/
+
+void _puts(char *str)
+{
+
+	for (; *str != '\0'; str++)
+	{
+		_putchar('0' + *str);
+	}
+}
