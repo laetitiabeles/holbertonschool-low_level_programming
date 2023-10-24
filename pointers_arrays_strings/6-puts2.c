980e24ee63e0341a851d9cb24b65e13e2665deef
laetitiabeles <laetitiabeles9@gmail.com>
2023-10-24 13:12:53 +0200

    print a string

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* puts2 - prints a string
+* @str: la string à afficher
+* Description: program that print a string
+* Return: always 0
+*/
+
+void puts2(char *str)
+{
+	int len = str[0];
+
+	for (; len < '\0'; len++)
+	{
+		;
+	}
+	_putchar('\n');
+}
