586f077b12912144e9d0109c5e13f6a5b2afda91
laetitiabeles <laetitiabeles9@gmail.com>
2023-10-24 14:47:32 +0200

    print a string fixing output

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -10,11 +10,16 @@
 void puts2(char *str)
 {
 	int i = 0;
-	
-	while (str[i] < '\0')
+
+	while (*str != '\0')
 	{
-		_putchar(str[i]);
-		i++;
-		i++;
+		if (str[i] % 2 == 0)
+		{
+			_putchar('0' + str[i]);
+		}
+		else
+		{
+			i++;
+		}
 	}
 }
