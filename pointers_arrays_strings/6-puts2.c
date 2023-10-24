5ee741c0b7f2c9cc3c1d64a9947c2740c86db46c
laetitiabeles <laetitiabeles9@gmail.com>
2023-10-24 15:13:30 +0200

    print a string fixing output

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,26 +1,35 @@
 #include "main.h"
 
 /**
-* puts2 - prints a string
+* _strlen - prints the length of a string
 * @str: la string à afficher
 * Description: program that print a string
 * Return: always 0
+*/
+
+int _strlen(char *str)
+{
+	int l = 0;
+
+	for (; *str != '\0'; str++)
+	{
+		l++;
+	}
+	return (l);
+}
+
+/**
+* puts2 - puts 1 char sur 2
+* @str: la string à afficher
 */
 
 void puts2(char *str)
 {
 	int i = 0;
 
-	while (*str != '\0')
+	for (; i < _strlen(str); i = i + 2)
 	{
-		if (str[i] % 2 == 0)
-		{
-			_putchar('0' + str[i]);
-		}
-		else
-		{
-			i++;
-		}
-		i++;
+		_putchar(str[i]);
 	}
+	_putchar('\n');
 }
