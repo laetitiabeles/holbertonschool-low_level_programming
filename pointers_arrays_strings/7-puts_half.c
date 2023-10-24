79ca43e455970880c3bbf50ff6beea88bcc76f06
laetitiabeles <laetitiabeles9@gmail.com>
2023-10-24 15:27:41 +0200

    print second half of the string

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+* _strlen - Entry point
+* @str: pointeur de str
+* Description: program that print the lenght of a string
+* Return: always 0
+*/
+
+int _strlen(char *str)
+{
+	int len = 0;
+
+	for (; *str != '\0'; str++)
+	{
+		len++;
+	}
+	return (len);
+}
+
+/**
+* puts_half - affiche la moitié de la string
+* @str: la string a afficher
+*/
+
+void puts_half(char *str)
+{
+	int len = _strlen(str);
+	int moitie = len / 2;
+	int i;
+
+	for (i = moitie; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+}
