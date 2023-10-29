d1924312ea6e9bc6e0bf0da6ed4890e557cc0c90
laetitiabeles <laetitiabeles9@gmail.com>
2023-10-29 20:25:19 +0100

    convert a string to integer

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -3,13 +3,37 @@
 
 /**
 * _atoi - entry point
-* @s: caractere a check
-* Return: Always 0.
+* @s: caractere a convertir en entier
+* Return: valeur numérique de la chaine
 */
 
 int _atoi(char *s)
 {
-	printf("test");
-	return (*s);
+	int result = 0;
+	int signe = 1; /* signe du nombre */
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == ' ' || s[i] == '+')
+		{
+			i++;
+		}
+		else if (s[i] == '-')
+		{
+			signe = -1;
+			i++;
+		}
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			result = result * 10 + (s[i] - '0');
+			i++;
+		}
+		else
+		{
+			break;
+		}
+	}
+	return ((result * signe));
 }
 
