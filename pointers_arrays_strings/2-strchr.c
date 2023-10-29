2ac35ac2acecdf68dee95d44aeecfd23ab9e9b25
laetitiabeles <laetitiabeles9@gmail.com>
2023-10-29 21:19:19 +0100

    add comments

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -3,7 +3,7 @@
 
 /**
 *_strchr - locates a character in a string
-*@s: string
+*@s: pointeur vers la string dans laquelle on souhaite localiser c
 *@c: character
 *
 *Return: first occurrence or NULL
@@ -11,22 +11,22 @@
 
 char *_strchr(char *s, char c)
 {
-	for (; *s; s++)
+	for (; *s; s++) /* parcours les caractères de la chaîne pointée par s */
 	{
-		if (*s != c)
+		if (*s != c) /* vérifie que le caractère s n'est pas égal au c recherché */
 		{
 			continue;
 		}
-		else
+		else /* sinon si une occurence a été trouvée */
 		{
-			return (s);
+			return (s); /* renvoie un pointeur vers la position de l'occurence */
 		}
 	}
 
-	if (c == '\0')
+	if (c == '\0') /* vérifie si c est NULL */
 	{
 		return (s);
 	}
 
-	return (NULL);
+	return (NULL); /* si aucune correspondance n'a été trouvée */
 }
