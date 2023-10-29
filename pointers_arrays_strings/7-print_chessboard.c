ed25f7f4472d65e474704955e5bc76932de3aa05
laetitiabeles <laetitiabeles9@gmail.com>
2023-10-29 20:03:00 +0100

    program that print a chessboard

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*print_chessboard - function that prints the chessboard
+*@a: chessboard
+*
+*Return: void
+*/
+
+void print_chessboard(char (*a)[8])
+{
+	int l, c;
+
+	for (l = 0; l < 8; l++)
+	{
+		for (c = 0; c < 8; c++)
+		{
+			_putchar(a[l][c]);
+		}
+		_putchar('\n');
+	}
+}
