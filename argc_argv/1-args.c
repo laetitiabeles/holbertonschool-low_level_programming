855db8014909c276462e7406f170e93ca53b62f2
laetitiabeles <laetitiabeles9@gmail.com>
2023-11-02 21:00:19 +0100

    program that print the number of arg

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+* @argc: int
+* @argv: char
+* Return: Always 0
+*/
+
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
