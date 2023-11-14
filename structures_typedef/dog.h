a4c861085a1149e0181e82316d03de67cc5a812d
laetitiabeles <laetitiabeles9@gmail.com>
2023-11-14 12:41:25 +0100

    poppy task

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+*/
+
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
+
+typedef struct dog dog_t;
+
+#endif
