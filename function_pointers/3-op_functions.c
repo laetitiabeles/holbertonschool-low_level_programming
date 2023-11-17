b02334b8dfeef7a5b7849b8acd2a848f72efc671
laetitiabeles <laetitiabeles9@gmail.com>
2023-11-17 16:46:29 +0100

    file with all functions for the program

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,64 @@
+/**
+* op_add - add two int
+* @a: first int to add
+* @b: second int to add
+*
+* Return: result of the two int
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* op_sub - sub two int
+* @a: first int to sub
+* @b: second int to sub
+*
+* Return: result of the two int
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* op_mul - multiply two int
+* @a: first int to multiply
+* @b: second int to multiply
+*
+* Return: result of the two int
+*/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* op_div - divide two int
+* @a: first int to divide
+* @b: second int to divide
+*
+* Return: result of the two int
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+* op_mod - modulo
+* @a: first int
+* @b: second int
+*
+* Return: result of the two int
+*/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
