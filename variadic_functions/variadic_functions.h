0db8c9980b84c7c219773e04e85b54ef5a6ae82d
laetitiabeles <laetitiabeles9@gmail.com>
2023-11-17 10:49:56 +0100

    Function that returns the sum of all its parameter

---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif
