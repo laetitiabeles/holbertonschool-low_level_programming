ae171e77dcf9d7a869c933cebb78ddd06cc5bf8f
laetitiabeles <laetitiabeles9@gmail.com>
2023-11-29 16:24:36 +0100

    fix the while loop

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -28,12 +28,12 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	}
 	else
 	{
-		temp = *head;
 		while (temp->next != NULL)
 		{
 			temp = temp->next;
 		}
 		temp->next = newNode;
+		newNode->prev = temp;
 	}
 	return (newNode);
 }
