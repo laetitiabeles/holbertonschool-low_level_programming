7b6810192937767fd03608a19c76045a4a4889c6
laetitiabeles <laetitiabeles9@gmail.com>
2023-11-29 14:51:04 +0100

    function that free a list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+* free_list - free linked list
+* @head: given linked list
+*/
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
