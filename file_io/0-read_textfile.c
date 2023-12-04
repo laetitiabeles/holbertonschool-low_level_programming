bb177a1e650cef472aef8ec18f1718eb1b0a4812
laetitiabeles <laetitiabeles9@gmail.com>
2023-12-04 14:50:15 +0100

    function that reads a text file and print

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+* read_textfile - write a part of file
+* @filename: given file
+* @letters: number of letters to print
+* Return: number of written letters
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+	int file, readFile, writeFile;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	file = open(filename, O_RDONLY);
+	if (file < 0)
+		return (0);
+
+	buffer = malloc(letters * sizeof(filename));
+	if (buffer == NULL)
+		close(file);
+		return (0);
+
+	readFile = read(file, buffer, letters);
+	if (readFile < 0)
+		free(buffer);
+		close(file);
+		return (0);
+
+	writeFile = write(STDOUT_FILENO, buffer, readFile);
+	if (writeFile < 0 || writeFile != readFile)
+		free(buffer);
+		close(file);
+		return (0);
+
+	free(buffer);
+	close(file);
+
+	return (writeFile);
+}
