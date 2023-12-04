0a71a3356029e534b4ff375442ebbe62559b7256
laetitiabeles <laetitiabeles9@gmail.com>
2023-12-04 14:51:51 +0100

    fixing gcc issues

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -14,28 +14,37 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buffer;
 
 	if (filename == NULL)
+	{
 		return (0);
+	}
 
 	file = open(filename, O_RDONLY);
 	if (file < 0)
+	{
 		return (0);
+	}
 
 	buffer = malloc(letters * sizeof(filename));
 	if (buffer == NULL)
+	{
 		close(file);
 		return (0);
-
+	}
 	readFile = read(file, buffer, letters);
 	if (readFile < 0)
+	{
 		free(buffer);
 		close(file);
 		return (0);
+	}
 
 	writeFile = write(STDOUT_FILENO, buffer, readFile);
 	if (writeFile < 0 || writeFile != readFile)
+	{
 		free(buffer);
 		close(file);
 		return (0);
+	}
 
 	free(buffer);
 	close(file);
