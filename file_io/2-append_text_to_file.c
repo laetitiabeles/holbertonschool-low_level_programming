aa1ebfd6b5ded847e95850ee7d9b9ebd3010cfca
laetitiabeles <laetitiabeles9@gmail.com>
2023-12-05 11:01:48 +0100

    fixing initialization of file var

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -14,7 +14,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	open(filename, O_RDWR | O_APPEND);
+	file = open(filename, O_RDWR | O_APPEND);
 	if (file < 0)
 		return (-1);
 
